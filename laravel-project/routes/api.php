137430269d08c9a6360fd5811c6dab9954e15e09
Wyatt VO <votranquy96@gmail.com>
2024-03-10 23:35:30 +0700

    Add route teacher api

--- laravel-project/routes/api.php
+++ laravel-project/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\API\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,5 +21,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::apiResource('student', StudentController::class);
 Route::resource('products', ProductController::class);
+//API route để đăng ký
+Route::post('/login', [AuthController::class, 'register']);
+//API route để đăng nhập
+Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+});
+// API route thoát
+Route::post('/logout', [AuthController::class, 'logout']);
+});
+//API route để đăng ký
+Route::post('/dangky', [AuthController::class, 'register']);
+//API route để đăng nhập
+Route::post('/dangnhap', [AuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+// API route thoát
+Route::post('/logout', [AuthController::class, 'logout']);
+});
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+Route::resource('teachers', ProductController::class);
+
+
 
 
