4859afdc422b3e32ff3f607fde9b7a9351cb1c8d
Wyatt VO <votranquy96@gmail.com>
2024-03-21 22:23:35 +0700

    TB-20240321: Install Vue using vite

---
+++ vite-project/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import axios, {isCancel, AxiosError} from 'axios';
+
+// console.log(axios.isCancel('something'));
+
+createApp(App).mount('#app')
